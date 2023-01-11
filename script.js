85cf2c1e43b101a1d9e4046f4e89e1b2bbec2d67
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2023-01-11 16:45:11 +0100

    change balance with value from prompt

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('JS file')
+let loan = 0
+let balanceFromStart = 200 + ' ' + 'kr'
+
+let balance = document.getElementById('balance')
+balance.innerHTML = balanceFromStart
+
+console.log(loan)
+console.log(balance)
+
+function getALoan() {
+   loan = prompt('Fill in the amount you want to have?')
+   console.log(loan)
+   balance.innerHTML = loan + ' ' + 'kr'
+}
+
+
+
