81c41eb083499fce5b3276a73dcc48f0ca3aeb8c
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2023-01-12 14:05:10 +0100

    check if hasLoan

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let loan = 0
-let balanceFromStart = 200 + ' ' + 'kr'
+let loan = 0;
+let balanceFromStart = 200 + " " + "kr";
+let hasLoan = false;
 
-let balance = document.getElementById('balance')
-balance.innerHTML = balanceFromStart
+let balance = document.getElementById("balance");
+let loanBtn = document.getElementById("loanBtn");
 
-console.log(loan)
-console.log(balance)
+balance.innerHTML = balanceFromStart;
 
-function getALoan() {
-   loan = prompt('Fill in the amount you want to have?')
-   console.log(loan)
-   balance.innerHTML = loan + ' ' + 'kr'
-}
+console.log(loan);
+console.log(balance);
 
-
-
+loanBtn.addEventListener("click", function () {
+  loan = prompt("Fill in the amount you want to have?");
+  if (hasLoan == false) {
+    balance.innerHTML = loan + " " + "kr";
+    hasLoan = true;
+  } else {
+    alert("You already have a loan");
+  }
+});
