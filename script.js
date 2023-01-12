bbf0f90ee909e55afcadfaaf413f8a9e7f1c7af6
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2023-01-12 14:22:38 +0100

    can't loan the double of balance

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 let loan = 0;
-let balanceFromStart = 200 + " " + "kr";
+let balanceValue = 200;
 let hasLoan = false;
 
 let balance = document.getElementById("balance");
 let loanBtn = document.getElementById("loanBtn");
 
-balance.innerHTML = balanceFromStart;
+balance.innerText = balanceValue;
 
 console.log(loan);
-console.log(balance);
+console.log(balanceValue);
 
 loanBtn.addEventListener("click", function () {
-  loan = prompt("Fill in the amount you want to have?");
-  if (hasLoan == false) {
-    balance.innerHTML = loan + " " + "kr";
-    hasLoan = true;
-  } else {
-    alert("You already have a loan");
-  }
+  getLoan();
 });
+
+function getLoan() {
+  loan = prompt("Fill in the amount you want to have?");
+
+  if (hasLoan == false && loan < balanceValue) {
+    balance.innerHTML = loan;
+    balanceValue = loan;
+    hasLoan = true;
+  } else if (hasLoan == true) {
+    alert("You already have a loan");
+  } else if (loan >= balanceValue) {
+    alert("you cant borrow more money than you have");
+  }
+}
