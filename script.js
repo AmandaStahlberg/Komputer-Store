3fe66629ee24e14354af6d1460feb8ec5d02bb56
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2023-01-16 10:31:59 +0100

    all logic is working and fetch image is there

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let loan = 0;
-let balanceValue = 200;
+let balanceValue = 2000;
 let hasLoan = false;
 let salary = 0;
 let laptops = [];
@@ -15,6 +15,8 @@ const featuresElement = document.getElementById("features");
 const titleElement = document.getElementById("laptopTitle");
 const textElement = document.getElementById("laptopDescription");
 const priceElement = document.getElementById("laptopPrice");
+const imageContainer = document.getElementById("imgContainer");
+let imageElement = document.createElement("img");
 
 const loanBtn = document.getElementById("loanBtn");
 const loanBtnDiv = document.getElementById("loanBtnDiv");
@@ -23,9 +25,9 @@ const bankBtn = document.getElementById("bankBtn");
 const workBtn = document.getElementById("workBtn");
 const buyBtn = document.getElementById("buyBtn");
 
-balance.innerText = balanceValue;
-salaryToShow.innerHTML = salary;
-loanToPay.innerHTML = loan;
+balance.innerText = formatCurrency(balanceValue);
+salaryToShow.innerHTML = formatCurrency(salary);
+loanToPay.innerHTML = formatCurrency(loan);
 
 loanDiv.style.display = "none";
 repayLoanDiv.style.display = "none";
@@ -35,7 +37,6 @@ loanBtn.addEventListener("click", function () {
 });
 
 repayLoanBtn.addEventListener("click", function () {
-  console.log("repay btn");
   repayLoan();
 });
 
@@ -47,24 +48,20 @@ workBtn.addEventListener("click", function () {
   getSalaryFromWork();
 });
 
-function repayLoan() {
-  if (salary >= loan) {
-    console.log("betala av hela lånet och gör hasloan false");
-  } else if ((salary = 0 || !salary)) {
-    console.log("hej du ha ringa pengar");
-  } else if (salary < loan) {
-    console.log("betala av så mkt du kan men ändra inte hasloan");
-  }
+function formatCurrency(num) {
+  return new Intl.NumberFormat().format(num);
 }
 
 function getLoan() {
   loan = prompt("Fill in the amount you want to have?");
 
   if (hasLoan == false && loan < balanceValue) {
-    balance.innerHTML = Number(loan) + Number(balanceValue);
-    balanceValue = loan;
+    balanceValue = Number(loan) + Number(balanceValue);
+    balance.innerHTML = formatCurrency(balanceValue);
+    console.log(balanceValue);
+    // balanceValue = loan;
     hasLoan = true;
-    loanToPay.innerHTML = loan;
+    loanToPay.innerHTML = formatCurrency(loan);
     loanDiv.style.display = "flex";
     repayLoanDiv.style.display = "flex";
     loanBtnDiv.style.display = "none";
@@ -75,36 +72,74 @@ function getLoan() {
   }
 }
 
+function repayLoan() {
+  let loanToRemove = loan;
+  // let updatedLoan = 0;
+
+  if (salary >= loan) {
+    balanceValue = Number(balanceValue) - Number(loanToRemove);
+    salary = Number(salary) - Number(loanToRemove);
+    salaryToShow.innerHTML = formatCurrency(salary);
+    loanToRemove = 0;
+    loanToPay.innerText = formatCurrency(loanToRemove);
+    balance.innerText = formatCurrency(balanceValue);
+    hasLoan = false;
+    repayLoanDiv.style.display = "none";
+    loanBtnDiv.style.display = "flex";
+  } else if (salary < loan && salary != 0) {
+    let updatedLoan = Number(loan) - Number(salary);
+    balanceValue = Number(balanceValue) - Number(updatedLoan);
+    console.log(balanceValue);
+    balance.innerText = formatCurrency(balanceValue);
+    loanToPay.innerText = formatCurrency(updatedLoan);
+    loan = updatedLoan;
+    salaryToShow.innerHTML = formatCurrency(salary);
+  }
+  if (salary === 0) {
+    alert("Go to work and get some money to repay your loan!!");
+  }
+  if (loan === 0) {
+    repayLoanDiv.style.display = "none";
+    loanBtnDiv.style.display = "flex";
+    loanDiv.style.display = "none";
+    hasLoan = false;
+    console.log("hej");
+  }
+}
+
 function getSalaryFromWork() {
   salary = Number(salary) + 100;
-  salaryToShow.innerHTML = salary;
+  salaryToShow.innerHTML = formatCurrency(salary);
 }
 
 function transferMoneyToBalance() {
   if (!loan) {
     balanceValue += salary;
-    balance.innerHTML = balanceValue;
+    balance.innerHTML = formatCurrency(balanceValue);
 
     salary = 0;
-    salaryToShow.innerHTML = salary;
+    salaryToShow.innerHTML = formatCurrency(salary);
   } else {
     let moneyToLoan = salary * 0.1;
     loan = Number(loan) - Number(moneyToLoan);
-    loanToPay.innerHTML = loan;
+    loanToPay.innerHTML = formatCurrency(loan);
 
     let moneyToBalance = salary * 0.9;
     balanceValue = Number(balanceValue) + Number(moneyToBalance);
-    balance.innerHTML = balanceValue;
+    balance.innerHTML = formatCurrency(balanceValue);
 
     salary = 0;
-    salaryToShow.innerHTML = salary;
+    salaryToShow.innerHTML = formatCurrency(salary);
   }
 }
 
 fetch("https://hickory-quilled-actress.glitch.me/computers")
   .then((response) => response.json())
   .then((data) => (laptops = data))
-  .then((laptops) => addLaptopsToMenu(laptops));
+  .then((laptops) => addLaptopsToMenu(laptops))
+  .catch((error) => {
+    console.log("something went wrong", error);
+  });
 
 const addLaptopsToMenu = (laptops) => {
   laptops.forEach((x) => addLaptopToMenu(x));
@@ -112,6 +147,8 @@ const addLaptopsToMenu = (laptops) => {
   titleElement.innerText = laptops[0].title;
   textElement.innerText = laptops[0].description;
   priceElement.innerText = laptops[0].price;
+  imageElement.src = `https://hickory-quilled-actress.glitch.me/${laptops[0].image}`;
+  imageContainer.append(imageElement);
 };
 
 const addLaptopToMenu = (laptop) => {
@@ -126,14 +163,21 @@ const handleLaptopMenuChange = (e) => {
   featuresElement.innerText = selectedLaptop.specs;
   titleElement.innerText = selectedLaptop.title;
   textElement.innerText = selectedLaptop.description;
-  priceElement.innerText = selectedLaptop.price;
+  priceElement.innerText = formatCurrency(selectedLaptop.price);
+
+  imageElement.src = `https://hickory-quilled-actress.glitch.me/${selectedLaptop.image}`;
+  imageContainer.append(imageElement);
 };
 
 const handleBuyLaptop = () => {
   const selectedLaptop = laptops[laptopsElement.selectedIndex].price;
   if (balanceValue >= selectedLaptop) {
-    alert("hej bra köp");
-  } else alert("hallå du e fattig");
+    alert(
+      `You are now a happy owner of an excellent laptop! And I´m gonna steal ${selectedLaptop} kr from your bank account.`
+    );
+    balanceValue = Number(balanceValue) - Number(selectedLaptop);
+    balance.innerHTML = formatCurrency(balanceValue);
+  } else alert("Hey, you can´t afford that! Go and get some more money!");
 };
 
 laptopsElement.addEventListener("change", handleLaptopMenuChange);
